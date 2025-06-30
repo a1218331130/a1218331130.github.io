037a0ee488133a19770c09f8fbb99424c765d8f2
@weituo123 <1218331130@qq.com>
2025-06-30 18:19:12 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+
+<template>
+  <Valid v-if="showCarNo" />
+  <CarForm v-if="!showCarNo" :isMain="isMain" />
+</template>
+<script setup>
+import Valid from "./components/valid.vue";
+import CarForm from "./components/form.vue";
+import { ref, onMounted } from "vue";
+let showCarNo = ref(false);
+let isMain = ref(false);
+onMounted(() => {
+  const carNo = localStorage.getItem("carNo");
+  let href = window.location.href;
+  isMain.value = href.includes("main");
+
+  if (isMain.value) {
+    return;
+  }
+  if (!carNo) {
+    showCarNo.value = true;
+  } else {
+    let href = window.location.href;
+
+    debugger;
+  }
+});
+</script>
+<style scoped>
+</style>
---
+++ src/components/form.vue
@@ -0,0 +1,55 @@
+
+<template>
+  <van-form @submit="onSubmit">
+    <van-cell-group inset>
+      <van-field
+        v-model="username"
+        name="username"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="password"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+    </van-cell-group>
+    <div style="margin: 16px">
+      <van-button round block type="primary" native-type="submit">
+        提交
+      </van-button>
+    </div>
+  </van-form>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+let showCarNo = ref(false);
+defineProps({
+  isMain: Boolean,
+});
+onMounted(() => {
+  // const carNo = localStorage.getItem("carNo");
+  // if (!carNo) {
+  //   debugger;
+  //   showCarNo.value = true;
+  // } else {
+  // }
+});
+const username = ref("");
+const password = ref("");
+const onSubmit = (values) => {
+  console.log("submit", values);
+};
+</script>
+
+
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
